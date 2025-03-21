b5d7609e51a1abc7ca5995e68674b4110a43ffbf
whiteheadbella <146896022+whiteheadbella@users.noreply.github.com>
2025-03-21 13:36:12 -0500

    Initial commit

--- EmployeeSystem/settings.py
+++ EmployeeSystem/settings.py
@@ -9,31 +9,22 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
-import dj_database_url
 
 from pathlib import Path
-from dotenv import load_dotenv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR/".eVar", ".env"))
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-$i(%=7$^6$k+utk&x0ut70&dvm$*@f*zg0-lwl3$ceg&1!%cw8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS").split()
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -78,17 +69,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EmployeeSystem.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'EmployeeSystem',  
+        'NAME': 'EmployeeSystem',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
@@ -98,7 +88,6 @@ DATABASES = {
         }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -118,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -129,7 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- EmployeeSystem/wsgi.py
+++ EmployeeSystem/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'django' program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EmployeeSystem.settings')
 
+# Get the WSGI application for serving the project
 application = get_wsgi_application()
