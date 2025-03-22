f1c6f4bac340225035362894d6dff9ac4cbdc6f5
whiteheadbella <146896022+whiteheadbella@users.noreply.github.com>
2025-03-21 22:58:46 -0500

    Update: Set DEBUG=False, add requirements.txt, update vercel.json

--- EmployeeSystem/settings.py
+++ EmployeeSystem/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$i(%=7$^6$k+utk&x0ut70&dvm$*@f*zg0-lwl3$ceg&1!%cw8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
